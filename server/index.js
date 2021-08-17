493b06d55743c1b1a345be297e4b1952942708ce
Shubham Patidar <shubhampatidar904@gmail.com>
2021-08-17 21:36:14 +0530

    Environment varaible added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://Shubham:DRKscZeJJnKVGu9D@cluster0.fa8bb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.REACT_APP_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
